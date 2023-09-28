888da1373971de8cef6c656939e2513f12ee8147
Maxboxx <68467736+Maxboxx@users.noreply.github.com>
2023-09-29 00:28:14 +0200

    Added array length to types

--- Structs.h
+++ Structs.h
@@ -42,27 +42,46 @@ namespace Kiwi {
 		/// The type name.
 		Boxx::String name;
 
-		Type() : pointers(0), name("") {}
-		explicit Type(const Boxx::String& type) : pointers(0), name(type) {}
-		Type(Boxx::UInt pointers, const Boxx::String& type) : pointers(pointers), name(type) {}
+		/// The array length.
+		Boxx::UInt len;
+
+		Type() : pointers(0), name(""), len(1) {}
+		explicit Type(const Boxx::String& type) : pointers(0), name(type), len(1) {}
+		Type(Boxx::UInt pointers, const Boxx::String& type) : pointers(pointers), name(type), len(1) {}
+		Type(const Boxx::String& type, Boxx::UInt len) : pointers(0), name(type), len(len) {}
+		Type(Boxx::UInt pointers, const Boxx::String& type, Boxx::UInt len) : pointers(pointers), name(type), len(len) {}
 		~Type() {}
 
 		/// Converts the type to kiwi.
 		Boxx::String ToKiwi() const {
-			return Name::ToKiwi(name) + Boxx::String("*").Repeat(pointers);
+			Boxx::String str = Name::ToKiwi(name);
+			
+			if (len != 1) {
+				str += '[' + Boxx::String::ToString(len) + ']';
+			}
+
+			if (pointers) {
+				str += Boxx::String("*").Repeat(pointers);
+			}
+
+			return str;
 		}
 
 		bool operator==(const Type& type) const {
-			return pointers == type.pointers && name == type.name;
+			return pointers == type.pointers && name == type.name && len == type.len;
 		}
 
 		bool operator!=(const Type& type) const {
-			return pointers != type.pointers || name != type.name;
+			return pointers != type.pointers || name != type.name || len != type.len;
 		}
 
 		bool operator<(const Type& type) const {
 			if (pointers != type.pointers) {
 				return pointers < type.pointers;
+			}
+
+			if (len != type.len) {
+				return len < type.len;
 			}
 
 			return name < type.name;
